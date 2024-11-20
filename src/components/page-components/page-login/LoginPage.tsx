0cf7addfbef9a18c964d6d399442286f988b8ac5
99slayer <21979862+99slayer@users.noreply.github.com>
2024-11-20 15:00:01 -0700

    feat: Add a guest login info element

--- src/components/page-components/page-login/LoginPage.tsx
+++ src/components/page-components/page-login/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import Component from '../../Component';
 import { user } from '../../../api/api';
 import { AppContext } from '../../../App';
@@ -14,6 +14,7 @@ function LoginPage() {
 	const navigate = useNavigate();
 	const loginRef = useRef<HTMLDialogElement>(null);
 	const signupRef = useRef<HTMLDialogElement>(null);
+	const [infoHover, setInfoHover] = useState<boolean>(false);
 
 	function toggle(ref: React.RefObject<HTMLDialogElement>): void {
 		const form: HTMLFormElement | null = ref.current!.querySelector('form');
@@ -27,7 +28,7 @@ function LoginPage() {
 	}
 
 	return (
-		<div className='flex justify-center'>
+		<div className='flex flex-col items-center justify-center gap-2'>
 			<div className='mt-[220px] p-6 flex flex-col rounded-lg bg-gray-onyx'>
 				<div className='flex flex-col justify-evenly gap-4'>
 					<button
@@ -42,25 +43,42 @@ function LoginPage() {
 					>
 						SIGNUP
 					</button>
-					<button
-						className='px-3 rounded-full hover:text-white bg-gray-outer-space'
-						onClick={async (e) => {
-							const res: Response = await user.createGuestUser(e);
-							const data: ReturnDataInterface = await res.json();
-
-							if (data.username) setAppUsername(data.username);
-							if (data.nickname) setAppNickname(data.nickname);
-							if (data.pfp) setAppPfp(data.pfp);
-							if (res.ok) navigate('/main');
-						}}
-					>
-						LOGIN AS GUEST
-					</button>
+					<div className='flex items-stretch'>
+						<button
+							className='px-3 rounded-l-full hover:text-white bg-gray-outer-space'
+							onClick={async (e) => {
+								const res: Response = await user.createGuestUser(e);
+								const data: ReturnDataInterface = await res.json();
+								if (data.username) setAppUsername(data.username);
+								if (data.nickname) setAppNickname(data.nickname);
+								if (data.pfp) setAppPfp(data.pfp);
+								if (res.ok) navigate('/main');
+							}}
+						>
+							LOGIN AS GUEST
+						</button>
+						<div
+							className='px-[3px] flex justify-center items-center rounded-r-full bg-blue-500 cursor-help'
+							onMouseOver={() => setInfoHover(true)}
+							onMouseLeave={() => setInfoHover(false)}
+						>
+							<span className="material-symbols-outlined text-[22px] text-white font-semibold">
+								info
+							</span>
+						</div>
+					</div>
 				</div>
 				<div>
 					<Component.LoginModal ref={loginRef} toggle={toggle} />
 					<Component.SignupModal ref={signupRef} toggle={toggle} />
 				</div>
+			</div>
+			<div
+				className={`w-[245px] p-2 rounded-lg text-sm text-white bg-blue-500 ${infoHover ? 'opacity-100' : 'opacity-0'}`}
+			>
+				<p>
+					This will log you into a temporary guest account. You will be able to browse user accounts and posts, but you will not be able to interact with them or create your own.
+				</p>
 			</div>
 		</div>
 	);
